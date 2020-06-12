fee0a7fa31d1e6222d1cc4a5d4a73198dcc79169
Alex Gomes <alex.gomes.net3@gmail.com>
2020-06-11 21:49:04 -0300

    Create route List All appointments

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -8,6 +8,10 @@ class AppointmentsRepository {
     this.appointments = [];
   }
 
+  public allAppointments(): Appointment[] {
+    return this.appointments;
+  }
+
   public findByDate(date: Date): Appointment | null {
     const findAppointment = this.appointments.find((appointment) =>
       isEqual(date, appointment.date),
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,10 +1,20 @@
-import { Router } from 'express';
+import { Router, request, response } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.allAppointments();
+
+  if (appointments.length === 0) {
+    return response.status(404).json({ message: 'Appointments not found!' });
+  }
+
+  return response.json(appointments);
+})
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
