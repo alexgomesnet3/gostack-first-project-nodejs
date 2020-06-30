56c61a4d9f0a0ca8d852d19d4e4fe04be1e248ea
Alex Gomes <alex.gomes.net3@gmail.com>
2020-06-29 22:24:16 -0300

    Generating web token JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -30,8 +32,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password invalid combination!');
     }
 
+    const token = sign({}, '168514656e09f137b7c3eee59b7e47bd', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
-      user
+      user,
+      token,
     };
   }
 }
