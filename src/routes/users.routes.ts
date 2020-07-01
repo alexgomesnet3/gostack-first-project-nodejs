f8297d6519d45247c3253ebc314ccc5168adc7c3
Alex Gomes <alex.gomes.net3@gmail.com>
2020-07-01 14:01:07 -0300

    Config Muler and the Archives Upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -28,8 +31,14 @@ usersRouter.post('/', async (request, response) => {
   }
 })
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
-  return response.json({ message: true })
+usersRouter.patch('/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({
+      ok: true
+    })
 });
 
 export default usersRouter;
